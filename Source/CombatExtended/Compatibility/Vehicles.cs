ec7917736d33030a6e40352d740d8cdfdb2aef16
Logan Perkins <logan@github.lp-programming.com>
2022-08-14 14:56:11 -0700

    Use split assembly for vehicle support

--- Source/CombatExtended/Compatibility/Vehicles.cs
+++ Source/CombatExtended/Compatibility/Vehicles.cs
@@ -8,82 +8,49 @@ using System.Reflection.Emit;
 using System;
 using UnityEngine;
 using RimWorld;
-using Vehicles;
 
 
 namespace CombatExtended.Compatibility
 {
-    class Vehicles: IPatch
+    public class Vehicles: IPatch
     {
 	private static bool active = false;
+
+	public static List<Func<Pawn,Tuple<bool,Vector2>>> CollisionBodyFactorCallbacks = new List<Func<Pawn,Tuple<bool,Vector2>>>();
+	
 	public bool CanInstall()
         {
+	    Log.Message("Combat Extended :: Checking Vehicle Framework");
             if (!ModLister.HasActiveModWithName("Vehicle Framework"))
             {
                 return false;
             }
             return true;
         }
-	public IEnumerable<string> GetCompatList() {
-	    yield break;
-	}
+        public IEnumerable<string> GetCompatList() {
+            yield return "VehiclesCompat";
+        }
+
 
 	public void Install()
         {
-	    VehicleTurret.ProjectileAngleCE = ProjectileCE.GetShotAngle;
-	    VehicleTurret.LaunchProjectileCE = LaunchProjectileCE;
+	    Log.Message("Combat Extended :: Installing Vehicle Framework");
 	    active = true;
 	}
-	public static object LaunchProjectileCE(ThingDef projectileDef,
-						Vector2 origin,
-						LocalTargetInfo target,
-						VehiclePawn vehicle,
-						float shotAngle,
-						float shotRotation,
-						float shotHeight,
-						float shotSpeed)
-	{
-	    projectileDef = projectileDef.GetProjectile();
-	    ProjectileCE projectile = (ProjectileCE)ThingMaker.MakeThing(projectileDef, null);
-	    GenSpawn.Spawn(projectile, vehicle.Position, vehicle.Map);
 
-	    projectile.ExactPosition = origin;
-	    projectile.canTargetSelf = false;
-	    projectile.minCollisionDistance = 1;
-	    projectile.intendedTarget = target;
-	    projectile.mount = null;
-	    projectile.AccuracyFactor = 1;
-
-	    
-	    projectile.Launch(
-			      vehicle,
-			      origin,
-			      shotAngle,
-			      shotRotation,
-			      shotHeight,
-			      shotSpeed,
-			      vehicle);
-	    return projectile;
-	}
-
+	
 	public static bool GetCollisionBodyFactors(Pawn pawn, out Vector2 ret) {
 	    ret = new Vector2();
 	    if (active) {
-		return _GetCollisionBodyFactors(pawn, out ret);
-	    }
-	    else {
-		return false;
-	    }
-	}
-
-	private static bool _GetCollisionBodyFactors(Pawn pawn, out Vector2 ret) {
-	    ret = new Vector2();
-	    if (pawn is VehiclePawn vehicle) {
-		ret = new Vector2(1, vehicle.def.fillPercent);
-		return true;
+		foreach (Func<Pawn,Tuple<bool,Vector2>> f in CollisionBodyFactorCallbacks) {
+		    var r = f(pawn);
+		    if (r.Item1) {
+			ret = r.Item2;
+			return true;
+		    }
+		}
 	    }
 	    return false;
 	}
-
     }
 }
